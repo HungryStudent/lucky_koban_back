03d74818791001c14acd800a6637198ccc050b16
HungryStudent <nikita.efan.2016@mail.ru>
2022-10-03 18:13:48 +0300

    add requirments.txt

--- server/main.py
+++ server/main.py
@@ -4,6 +4,6 @@ from core.database import engine
 from endpoints import users, admin, items
 
 models.Base.metadata.create_all(bind=engine)
-app = FastAPI(prefix="/api")
+app = FastAPI()
 app.include_router(users.router, prefix="/user")
 app.include_router(items.router, prefix="/api/items")
