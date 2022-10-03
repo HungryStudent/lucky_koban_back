cef7c840553961c3dd80d8440395d14f5146cb34
HungryStudent <nikita.efan.2016@mail.ru>
2022-10-03 18:58:06 +0300

    add requirments.txt

--- server/main.py
+++ server/main.py
@@ -7,3 +7,8 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(users.router, prefix="/user")
 app.include_router(items.router, prefix="/api/items")
+
+
+@app.get('/api/get_me')
+async def get_cases():
+    return "oks"
