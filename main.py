b0430c8d31e7770ced6845d51166b4db2a187d23
HungryStudent <nikita.efan.2016@mail.ru>
2022-10-03 22:37:22 +0300

    correct fastapi object

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from core.database import engine
 from endpoints import users, admin, items
 
 models.Base.metadata.create_all(bind=engine)
-app = FastAPI(root_path="api", docs_url="/api/docs", openapi_url="api/openapi.json")
+app = FastAPI(root_path="api", docs_url="/api/docs", openapi_url="/api/openapi.json")
 app.include_router(users.router, prefix="/user")
 app.include_router(items.router, prefix="/api/items")
